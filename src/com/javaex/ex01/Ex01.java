43e98c02a722e1487d7ee0a170044c6aff6258f7
clz2024.sun <clz2024.sun@gmail.com>
2024-07-15 12:19:20 +0900

    2024-07-15

--- src/com/javaex/ex01/Ex01.java
+++ src/com/javaex/ex01/Ex01.java
@@ -10,24 +10,33 @@ public class Ex01 {
 
 	public static void main(String[] args) throws IOException{
 		
-		InputStream	in = new FileInputStream("C:\\javaStudy\\img.jpg");
+		// I/O스트림준비
+		InputStream in = new FileInputStream("C:\\javaStudy\\img.jpg");
 		OutputStream out = new FileOutputStream("C:\\javaStudy\\byteimg.jpg");
+		System.out.println("스트림준비완료");
 		
-		while (true) {
-
-			int data = in.read();
-			System.out.println(data);
+		//반복
+		while(true) {
 			
+			//데이터 읽기
+			int data = in.read();
+			System.out.println("data:" + data);
+			
+			//다 읽으면 반복문 빠져나감
 			if(data == -1) {
-				//System.out.println("다 읽었음");
+				System.out.println("읽기끝");
 				break;
 			}
 			
+			//데이터 쓰기
 			out.write(data);
+			
 		}
-		System.out.println("복사 끝");
-		out.close();
+		
+		System.out.println("프로그램 종료");
+		
 		in.close();
+		out.close();
 	}
 
 }
